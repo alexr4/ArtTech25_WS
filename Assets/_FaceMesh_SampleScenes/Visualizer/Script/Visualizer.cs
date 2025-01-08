c7525473e4facb1e89f30b1ef4de08f0a2a041fb
Alexandre Rivaux <alex@bonjour-lab.com>
2025-01-08 17:41:49 +0100

    Correct Face tracking
    → Correction on the winding order (to avoid having to set the shader as a Double Sided Shader)
    → Set mesh position into view space in GPU using Compute Shader (and try to keep it on GPU side but binding to NativeArray<Vec3> seems a bottleneck)
    → Add debug view to display vertex ico + index on view

--- Assets/_FaceMesh_SampleScenes/Visualizer/Script/Visualizer.cs
+++ Assets/_FaceMesh_SampleScenes/Visualizer/Script/Visualizer.cs
@@ -5,29 +5,50 @@ using Klak.TestTools;
 using MediaPipe.FaceMesh;
 using System.Collections.Generic;
 using System;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
+using Unity.Collections;
+using Unity.VisualScripting;
+using UnityEngine.Rendering;
+using Bonjour;
+using Mediapipe.BlazePose;
+using static Mediapipe.BlazePose.BlazePoseLandmarkFilters;
+using UnityEngine.SocialPlatforms.Impl;
 
 public sealed class Visualizer : MonoBehaviour
 {
     #region Editable attributes
-
+    [Header("AI params")]
     [SerializeField] RenderTexture _source = null;
     [Space]
     [SerializeField] ResourceSet _resources = null;
+
+    [Header("Rendering")]
+    public ComputeShader compute;
+
+    [Header("Debug section")]
     [SerializeField] Shader _shader = null;
     [Space]
     [SerializeField] RawImage _mainUI = null;
     [SerializeField] RawImage _faceUI = null;
     [SerializeField] RawImage _leftEyeUI = null;
     [SerializeField] RawImage _rightEyeUI = null;
-    
+    [Space]
+    [SerializeField] bool drawDebug;
+    [SerializeField] float debugIcoSize;
+    [SerializeField] Texture2D debugIco;
+
 
     #endregion
 
     #region Private members
 
-    FacePipeline _pipeline;
-    Material _material;
-    RenderTexture _outputTexture = null;
+    private FacePipeline _pipeline;
+    private Material _material;
+    private RenderTexture _outputTexture = null;
+    private ComputeShader _compute;
+    private ComputeBuffer vertexBuffer, normalBuffer;
+    private int kernelVertexHandle, kernelNormalHandle;
+    private Mesh mesh;
 
     #endregion
 
@@ -38,12 +59,22 @@ public sealed class Visualizer : MonoBehaviour
         _pipeline = new FacePipeline(_resources);
         _material = new Material(_shader);
         _outputTexture = new RenderTexture(1024, 1024, 0);
+
+        _compute = Instantiate(compute);
+        kernelVertexHandle = _compute.FindKernel("UpdateVertices");
+        kernelNormalHandle = _compute.FindKernel("UpdateNormals");
+
+        ResetWindingOrder();
     }
 
     void OnDestroy()
     {
         _pipeline.Dispose();
         Destroy(_material);
+
+        vertexBuffer?.Dispose();
+        normalBuffer?.Dispose();
+        if (_compute != null) Destroy(_compute);
     }
 
     void LateUpdate()
@@ -53,26 +84,30 @@ public sealed class Visualizer : MonoBehaviour
         // Processing on the face pipeline
         _pipeline.ProcessImage(_outputTexture);
 
-        // UI update
+        // UI update for debug
         _mainUI.texture = _outputTexture;
         _faceUI.texture = _pipeline.CroppedFaceTexture;
         _leftEyeUI.texture = _pipeline.CroppedLeftEyeTexture;
         _rightEyeUI.texture = _pipeline.CroppedRightEyeTexture;
 
-        //Update Mesh Filter
-        //bind GPU vertex buffer to CPU public vertex buffer 
-        Vector4[] vertexBuffer = new Vector4[_pipeline.RefinedFaceVertexBuffer.count] ;
+        //GPU-CPU Update Mesh
+        UpdateMeshGPU();
+        //UpdateMeshCPU();
+    }
+
+    private void UpdateMeshCPU()
+    {
+        if (_pipeline.RefinedFaceVertexBuffer == null) return;
+
+        Vector4[] vertexBuffer = new Vector4[_pipeline.RefinedFaceVertexBuffer.count];
         _pipeline.RefinedFaceVertexBuffer.GetData(vertexBuffer);
 
+
         // Get the MeshFilter component
-        MeshFilter meshFilter = GetComponent<MeshFilter>();
-        //var mv = float4x4.Translate(math.float3(-1f, -0.5f, 0)); // inly if we want to use matrix multiplication
-
-        if (meshFilter != null)
+        mesh = GetComponent<MeshFilter>().mesh;
+        if (mesh != null)
         {
-            // Get the mesh
-            Mesh mesh = meshFilter.mesh;
-
+           
             Vector3[] vertices = new Vector3[vertexBuffer.Length];
 
             // Modify the vertices (e.g., move each vertex upwards by 1 unit)
@@ -80,7 +115,7 @@ public sealed class Visualizer : MonoBehaviour
             {
                 //float4 viewVertex = math.mul(mv, vertexBuffer[i]);  // only if we want to use matrix multiplication
                 //Vector3 vertex = new Vector3(viewVertex.x, viewVertex.y, viewVertex.z);
-                
+
                 //remap to the center
                 float3 sampleVertex = new Vector3(vertexBuffer[i].x - .5f, vertexBuffer[i].y - .5f, vertexBuffer[i].z);
 
@@ -88,15 +123,55 @@ public sealed class Visualizer : MonoBehaviour
             }
 
             // Update the mesh with the new vertices
-            mesh.vertices = vertices;
+            mesh.SetVertices(vertices);
 
-            // Recalculate bounds and normals to ensure the mesh is correctly updated
-            //mesh.RecalculateBounds();
             mesh.RecalculateNormals();
+            // mesh.RecalculateBounds();
+
         }
     }
 
-    void Blit(Texture source, bool vflip = false)
+    private void UpdateMeshGPU()
+    {
+        if(_pipeline.RefinedFaceVertexBuffer == null) return;
+
+        //Update vertex position
+        Vector3[] vertices = mesh.vertices;
+
+        if(vertexBuffer == null) vertexBuffer = new ComputeBuffer(vertices.Length, sizeof(float) * 3, ComputeBufferType.Structured);
+        vertexBuffer.SetData(vertices);
+
+        _compute.SetBuffer(kernelVertexHandle, "vertexBuffer", vertexBuffer);
+        _compute.SetBuffer(kernelVertexHandle, "refinedFaceVertexBuffer", _pipeline.RefinedFaceVertexBuffer);
+
+        int threadVertexGroups = Mathf.CeilToInt(vertices.Length / 256.0f);
+        _compute.Dispatch(kernelVertexHandle, threadVertexGroups, 1, 1);
+
+        vertexBuffer.GetData(vertices);
+        mesh.vertices = vertices;
+
+        //Correct Normal
+        mesh.RecalculateNormals();
+        //mesh.RecalculateBounds();
+    }
+
+    private void ResetWindingOrder()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+
+        var indices = mesh.triangles;
+        var triangleCount = indices.Length / 3;
+        for (var i = 0; i < triangleCount; i++)
+        {
+            var tmp = indices[i * 3];
+            indices[i * 3] = indices[i * 3 + 1];
+            indices[i * 3 + 1] = tmp;
+        }
+        mesh.triangles = indices;
+    }
+
+
+    private void Blit(Texture source, bool vflip = false)
     {
         if (source == null) return;
 
@@ -112,7 +187,9 @@ public sealed class Visualizer : MonoBehaviour
         Graphics.Blit(source, _outputTexture, scale, offset);
     }
 
-    void OnRenderObject()
+    
+    //This methods doesn't render in HDRP
+    private void OnRenderObject()
     {
         // Main view overlay
         var mv = float4x4.Translate(math.float3(-0.875f, -0.5f, 0));
@@ -122,11 +199,14 @@ public sealed class Visualizer : MonoBehaviour
 
         // Face view
         // Face mesh
+        /*
         var fF = MathUtil.ScaleOffset(0.5f, math.float2(0.125f, -0.5f));
         _material.SetBuffer("_Vertices", _pipeline.RefinedFaceVertexBuffer);
         _material.SetPass(0);
         Graphics.DrawMeshNow(_resources.faceMeshTemplate, fF);
+        */
 
+        /*
         // Left eye
         var fLE = math.mul(fF, _pipeline.LeftEyeCropMatrix);
         _material.SetMatrix("_XForm", fLE);
@@ -140,14 +220,16 @@ public sealed class Visualizer : MonoBehaviour
         _material.SetBuffer("_Vertices", _pipeline.RawRightEyeVertexBuffer);
         _material.SetPass(3);
         Graphics.DrawProceduralNow(MeshTopology.Lines, 64, 1);
-
+        */
         // Debug views
         // Face mesh
+        /*
         var dF = MathUtil.ScaleOffset(0.5f, math.float2(0.125f, 0));
         _material.SetBuffer("_Vertices", _pipeline.RawFaceVertexBuffer);
         _material.SetPass(1);
         Graphics.DrawMeshNow(_resources.faceLineTemplate, dF);
-
+        */
+        /*
         // Left eye
         var dLE = MathUtil.ScaleOffset(0.25f, math.float2(0.625f, 0.25f));
         _material.SetMatrix("_XForm", dLE);
@@ -161,7 +243,31 @@ public sealed class Visualizer : MonoBehaviour
         _material.SetBuffer("_Vertices", _pipeline.RawRightEyeVertexBuffer);
         _material.SetPass(3);
         Graphics.DrawProceduralNow(MeshTopology.Lines, 64, 1);
+        */
     }
+    
 
+    #endregion
+
+    #region Draw Debug
+    private void OnGUI()
+    {
+        if (_pipeline != null && drawDebug)
+        {
+            for (int i = 0; i < mesh.vertices.Length - 1; i++)
+            {
+                Vector3 vertex = mesh.vertices[i];
+                Vector2 screenPosition = Camera.main.WorldToScreenPoint(vertex);
+                screenPosition.y = Screen.height - screenPosition.y;
+                GUI.DrawTexture(new Rect(screenPosition.x - debugIcoSize * .5f, screenPosition.y - debugIcoSize * .5f, debugIcoSize, debugIcoSize), debugIco);
+                GUI.Label(new Rect(screenPosition.x + debugIcoSize * .75f, screenPosition.y, 100, 20), $"<color=#ffffff>{i}.</color>", new GUIStyle()
+                {
+                    fontSize = 10,
+                    fontStyle = FontStyle.Bold
+                });
+            }
+        }
+
+    }
     #endregion
 }
